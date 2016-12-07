e4b6eeb831e4cd22c5e2df4e0a227264f5d0abe7
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-12-06 21:14:13 -1000

    purchaselaptop block passsed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -456,7 +456,13 @@ class Person {
  * @return {String}
  *
  */
-
+function purchaseLaptop(laptop){
+  if(laptopCosts.hasOwnProperty(laptop)){
+    return laptopCosts[laptop].toString();
+  } else {
+    return -1;
+  }
+}
 
 /* Step 29
  *
