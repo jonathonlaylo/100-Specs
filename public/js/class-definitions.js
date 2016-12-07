fe1d827710f05f8d3d0acdfecf2046da6e5aae89
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-12-07 13:26:37 -1000

    98 passed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1394,8 +1394,8 @@ var shortStory = new House(1).isATallStory(6666);
  * and assign the values to each variable below.
  *
  */
-var kitchenLightsOn;
-var porchLightsOff;
+var kitchenLightsOn = new Lightbulb(true).flipSwitch('on');
+var porchLightsOff = new Lightbulb(false).flipSwitch('off');
 
 
  /* Step 99
